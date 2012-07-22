04455918bd2cf966bda837234d7c14d9b96d3d04
Felix Krause <flyx@isobeef.org>
2012-07-22 21:38:04 +0200

    Made sound less crappy

--- src/synthesizer.js
+++ src/synthesizer.js
@@ -2,10 +2,20 @@ Synthesizer = function() {
    this.tracks = [];
    this.events = [];
    this.time = 0;
+   this.context = new webkitAudioContext();
+}
+
+Synthesizer.prototype.process = function(e) {
+   for (var i = 0; i < this.tracks.length; i++) {
+      this.tracks[i].process(e, i, this.context.sampleRate);
+   }
 }
 
 Synthesizer.prototype.play = function() {
    var that = this;
+   this.node = this.context.createJavaScriptNode(1024, this.tracks.length, this.tracks.length);
+   this.node.onaudioprocess = function(e) { that.process(e) };
+   this.node.connect(this.context.destination);
    this.timer = setInterval(function() {
       that.step();
    }, 60);
@@ -14,13 +24,11 @@ Synthesizer.prototype.play = function() {
 
 Synthesizer.prototype.pause = function() {
    clearInterval(this.timer);
+   this.node.disconnect();
 }
 
 Synthesizer.prototype.reset = function() {
-   for (var i = 0; i < this.tracks.length; i++) {
-      this.tracks[i].reset();
-   }
-   this.x = 0;
+   this.time = 0;
 }
 
 Synthesizer.prototype.addTrack = function(track) {
@@ -49,34 +57,14 @@ Synthesizer.prototype.addEvent = function(time, track, synthEvent) {
 TrackInterface = {
    handleEvent : function(event) {
       // abstract method
-   },
-   start : function() {
-      if (!this.on) {
-         this.node.connect(this.context.destination);
-         this.on = true;
-      }
-   },
-   pause : function() {
-      if (this.on) {
-         this.node.disconnect();
-         this.on = false;
-      }
-   },
-   reset : function() {
-      // abstract method
    }
 }
 
-SineTrack = function(context,channel) {
+SineTrack = function() {
    var that = this;
-   this.context = context;
-   this.node = this.context.createJavaScriptNode(256, 10, 10);
-   this.node.onaudioprocess = function(e) { that.process(e) };
-   this.channel = channel;
    this.frequency = 440;
    this.next_frequency = 440;
    this.amplitude = 1;
-   this.sample_rate = this.context.sampleRate;
    this.x = 0;
    this.on = false;
 }
@@ -85,7 +73,7 @@ SineTrack.prototype = TrackInterface;
 
 SineTrack.prototype.handleEvent = function(event) {
    switch (event.name) {
-      case "on": this.x = 0; this.stop_request = false; this.start(); break;
+      case "on": this.x = 0; this.stop_request = false; this.damp = 0; this.on = true; break;
       case "off":
          this.stop_request = true;
          break;
@@ -99,39 +87,34 @@ SineTrack.prototype.handleEvent = function(event) {
    }
 }
 
-SineTrack.prototype.process = function(e) {
-   var data = e.outputBuffer.getChannelData(this.channel);
-   var stopping = false;
-   for (var i = 0; i < data.length; ++i) {
-      if (stopping) {
-         data[i] = 0;
-      } else {
-         data[i] = this.amplitude * Math.sin(this.x++ /
-               (this.sample_rate / (2 * Math.PI * this.frequency)));
-      }
-      if (this.next_frequency != this.frequency || this.stop_request) {
-         next_data = this.amplitude * Math.sin(
-               this.x / (this.sample_rate / (2 * Math.PI * this.frequency)));
+SineTrack.prototype.process = function(e, channel, sampleRate) {
+   var data = e.outputBuffer.getChannelData(channel);
+   
+   for (var i = 0; i < data.length; i++) {
+      if (this.on) {
+      
+         if (this.stop_request && this.damp > 0) {
+            this.damp -= 0.001;
+         } else if (!this.stop_request && this.damp < 1) {
+            this.damp += 0.001;
+         }
+         data[i] = this.amplitude * this.damp * Math.sin(this.x++ /
+               (sampleRate / (2 * Math.PI * this.frequency)));
+         
          if (this.next_frequency != this.frequency) {
+            next_data = this.amplitude * this.damp * Math.sin(this.x / 
+                     (sampleRate / (2 * Math.PI * this.frequency)));
             if (data[i] < 0.001 && data[i] > -0.001 && data[i] < next_data) {
                this.frequency = this.next_frequency;
                this.x = 0;
             }
          }
-         
-         if (this.stop_request) {
-            // xor
-            if ((next_data < 0) ? (data[i] >= 0) : (data[i] < 0)) {
-               stopping = true;
-            }
-         }
+      } else {
+         data[i] = 0;
       }
    }
-   if (stopping) {
-      this.pause();
+   
+   if (this.damp <= 0) {
+      this.on = false;
    }
-}
-
-SineTrack.prototype.reset = function(e) {
-   this.pause();
 }
